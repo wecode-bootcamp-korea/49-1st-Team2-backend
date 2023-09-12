2727e30b18e1a2a4eaec6ba454b8f4a348a6b0e3
B3lla <sjin09300@gmail.com>
2023-09-12 11:57:57 +0900

    fix: 로그인 -  주석 삭제

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const { loginController } = require('../controllers/loginController');
 
 router.post('/login', loginController);
-// verifyToken
 
 module.exports = router;
