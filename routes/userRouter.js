fa88178f4578805693fe5b4cd09eaa0e3573e3fc
Rooney Lee <kroonstazy@gmail.com>
2023-09-12 12:11:16 +0900

    fix: 회원가입 오타수정 & 기능 구현 중

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const userController = require('../controllers/singUpController');
+const signUpController = require('../controllers/signUpController');
 
 const router = express.Router();
 
--- services/signUpServices.js
+++ services/signUpServices.js
@@ -1,10 +1,7 @@
-const userDao = require('../models/userDao')
-
 const signUp = async (name, email, password, profileImage, nickname, phoneNumber, birthday) => {
-    // password validation using REGEX
-    const pwValidation = new RegExp(
-      '^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,20})'
-    );
+    
+    const pwValidation = /.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\|-].*/;
+
     if (!pwValidation.test(password)) {
       const err = new Error('PASSWORD_IS_NOT_VALID');
       err.statusCode = 409;
@@ -22,7 +19,67 @@ const signUp = async (name, email, password, profileImage, nickname, phoneNumber
       
         return createUser;
       };
+
+      const createUser = async ( name, email, password, profileImage, nickname, phoneNumber, birthday ) => {
+        try {
+          return await myDataSource.query(
+          `INSERT INTO users(
+            name,
+            email,
+            password
+            profile_image,
+                  nickname,
+                  phone_number,
+                  birthday
+          ) VALUES (?, ?, ?, ?);
+          `,
+          [ name, email, password, profileImage, nickname, phoneNumber,birthday ]
+          );
+        } catch (err) {
+          const error = new Error('INVALID_DATA_INPUT');
+          error.statusCode = 500;
+          throw error;
+        }
+      };
   
   module.exports = {
       signUp
   }
+
+
+//   const { DataSource } = require('typeorm');
+
+// const myDataSource = new DataSource({
+// 	type: process.env.TYPEORM_CONNECTION,
+//     host: process.env.TYPEORM_HOST,
+//     port: process.env.TYPEORM_PORT,
+//     username: process.env.TYPEORM_USERNAME,
+//     password: process.env.TYPEORM_PASSWORD,
+//     database: process.env.TYPEORM_DATABASE
+// })
+
+// const createUser = async ( name, email, password, profileImage, nickname, phoneNumber, birthday ) => {
+// 	try {
+// 		return await myDataSource.query(
+// 		`INSERT INTO users(
+// 			name,
+// 			email,
+// 			password
+// 			profile_image,
+//             nickname,
+//             phone_number,
+//             birthday
+// 		) VALUES (?, ?, ?, ?);
+// 		`,
+// 		[ name, email, password, profileImage, nickname, phoneNumber,birthday ]
+// 	  );
+// 	} catch (err) {
+// 		const error = new Error('INVALID_DATA_INPUT');
+// 		error.statusCode = 500;
+// 		throw error;
+// 	}
+// };
+
+// module.exports = {
+//   createUser
+// }
