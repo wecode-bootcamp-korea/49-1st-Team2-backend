a9ab632a26e9617a7aa46911b328386f66f0ec82
Rooney Lee <kroonstazy@gmail.com>
2023-09-12 12:14:43 +0900

    fix: 회원가입 - 오타수정 및 필요없는 파일 삭제

--- services/signUpServices.js
+++ services/signUpServices.js
@@ -1,4 +1,4 @@
-const signUp = async (name, email, password, profileImage, nickname, phoneNumber, birthday) => {
+const createUser = async (name, email, password, profileImage, nickname, phoneNumber, birthday) => {
     
     const pwValidation = /.*[!@#$%^&*()_+{}\[\]:;<>,.?~\\|-].*/;
 
@@ -41,45 +41,15 @@ const signUp = async (name, email, password, profileImage, nickname, phoneNumber
           throw error;
         }
       };
+
+
+
+
+
   
-  module.exports = {
-      signUp
-  }
+module.exports = {
+  createUser
+}
 
 
-//   const { DataSource } = require('typeorm');
 
-// const myDataSource = new DataSource({
-// 	type: process.env.TYPEORM_CONNECTION,
-//     host: process.env.TYPEORM_HOST,
-//     port: process.env.TYPEORM_PORT,
-//     username: process.env.TYPEORM_USERNAME,
-//     password: process.env.TYPEORM_PASSWORD,
-//     database: process.env.TYPEORM_DATABASE
-// })
-
-// const createUser = async ( name, email, password, profileImage, nickname, phoneNumber, birthday ) => {
-// 	try {
-// 		return await myDataSource.query(
-// 		`INSERT INTO users(
-// 			name,
-// 			email,
-// 			password
-// 			profile_image,
-//             nickname,
-//             phone_number,
-//             birthday
-// 		) VALUES (?, ?, ?, ?);
-// 		`,
-// 		[ name, email, password, profileImage, nickname, phoneNumber,birthday ]
-// 	  );
-// 	} catch (err) {
-// 		const error = new Error('INVALID_DATA_INPUT');
-// 		error.statusCode = 500;
-// 		throw error;
-// 	}
-// };
-
-// module.exports = {
-//   createUser
-// }
