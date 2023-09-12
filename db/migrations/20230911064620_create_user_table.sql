7dc16d06b6fcd17d92d428ca76500189e57b468f
B3lla <sjin09300@gmail.com>
2023-09-12 12:57:51 +0900

    chore: dbmate users table 데이터속성 수정

--- db/migrations/20230911064620_create_user_table.sql
+++ db/migrations/20230911064620_create_user_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `nickname` varchar(10) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(500) NOT NULL,
   `profile_image` text NOT NULL,
   `phone_number` varchar(20) DEFAULT NULL,
   `birth_day` date DEFAULT NULL,
