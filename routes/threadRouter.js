290959157423d2b3c3b7a453835fc40c3296ff6a
hyunjin <alfma123@naver.com>
2023-09-13 12:20:15 +0900

    refactor: 공통 - controllers, services 일관화

--- routes/threadRouter.js
+++ routes/threadRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { verifyToken } = require('../middlewares');
-const { createThreadController } = require('../controllers/threadController');
+const { threadControllers } = require('../controllers');
+const { createThreadController } = threadControllers;
 const router = express.Router();
 
 router.post('/', verifyToken, createThreadController);
