1d689b8843fe4c8b4184641494a0bc9f21ec1d99
Rooney Lee <kroonstazy@gmail.com>
2023-09-13 17:14:14 +0900

    test: 게시글(스레드) 생성 테스트 완료, 성공 - 코드수정 확인바람

--- controllers/threadController.js
+++ controllers/threadController.js
@@ -4,6 +4,9 @@ const { throwError } = require('../utils');
 
 const createThreadController = async (req, res, next) => {
   try {
+    console.log(req)
+    console.log(req.user)
+    console.log(req.body)
     const { id } = req.user;
     const { content } = req.body;
     if (!content) throwError(400, 'key error');
