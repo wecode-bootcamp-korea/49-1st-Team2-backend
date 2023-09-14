d0299a0c734568cbdf1bb1c1c98805f93f86b168
Rooney Lee <kroonstazy@gmail.com>
2023-09-14 14:41:36 +0900

    chore: dev pull 이후 확정 커밋

--- services/commentService.js
+++ services/commentService.js
@@ -4,13 +4,18 @@ const { commentDao } = require('../models');
 const { createCommentDao, updateCommentDao, deleteCommentDao} = commentDao;
 
 const createCommentService = async () => {
-
+    createCommentDao()
+    return { message : "comment created",
+...nickname,
+...}
 };
-const updateCommentService = async () => {
-
+const updateCommentService = async (id, body) => {
+    updateCommentDao(id, body);
+    return "comment updated";
 };
-const deleteCommentService = async () => {
-
+const deleteCommentService = async (id, threadId) => {
+    deleteCommentDao(id, threadId);
+    return "comment deleted";
 };
 
 
