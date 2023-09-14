445fcce6084a8293e43e033d6cdda7763a422c1e
B3lla <sjin09300@gmail.com>
2023-09-14 16:09:14 +0900

    fix: conflict merge

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
 const { getVerificationCodeDao, setNewPasswordDao } = require('./userDao');
-const { updateThreadDao } = require('./threadDao');
+const { updateThreadDao, deleteThreadDao } = require('./threadDao');
 
 module.exports = {
   userDao: { getVerificationCodeDao, setNewPasswordDao },
   threadDao: {
     updateThreadDao,
+    deleteThreadDao,
   },
 };
