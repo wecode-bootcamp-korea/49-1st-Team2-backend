6d8fce37b67aa515d2907dbe717dd4d03317b54f
hyunjin <alfma123@naver.com>
2023-09-14 15:37:22 +0900

    fix: viewThreadComment 원래버전으로 복구

--- services/threadService.js
+++ services/threadService.js
@@ -13,56 +13,36 @@ const createThreadService = async (id, content) => {
 
 const viewThreadService = async (id, next) => {
   try {
-    let viewThread = await dataSource.query(
+    let [viewThread] = await dataSource.query(
       `
       SELECT threads.id, users.nickname, users.profile_image, threads.user_id, threads.content, threads.created_at
-      FROM threads
-      LEFT JOIN users ON threads.user_id = users.id
+      FROM threads, users;
       `,
     );
 
-    let commentsMap = new Map();
-    for (let i = 0; i < viewThread.length; i++) {
-      let result = await dataSource.query(
-        `
-          SELECT threads.id, users.nickname, comments.comment, comments.created_at, comments.user_id AS comment_user_id
-          FROM threads
-          LEFT JOIN comments ON threads.id = comments.thread_id
-          LEFT JOIN users ON comments.user_id = users.id
-          WHERE threads.id = ?
-          `,
-        [viewThread[i].id],
-      );
-      if (result[0].nickname) {
-        commentsMap.set(viewThread[i].id, result);
-      }
-    }
+    const viewThreadComment = await dataSource.query(
+      `
+      SELECT threads.id, users.nickname, comments.content, comments.created_at, comments.user_id AS comment_user_id
+      FROM threads
+      LEFT JOIN comments ON threads.id = comments.thread_id
+      LEFT JOIN users ON comments.user_id = users.id
+      WHERE threads.id = ?
+      `,
+      [viewThread.id],
+    );
+
     return {
-      data: viewThread.map((outerData) => {
-        if (commentsMap.get(outerData.id)) {
-          return {
-            id: outerData.id,
-            nickname: outerData.nickname,
-            profileImage: outerData.profile_image,
-            content: outerData.content,
-            isMyPost: id === outerData.user_id,
-            // commentCount: arr[index].length,
-            comments: commentsMap.get(outerData.id).map((data) => {
-              return { ...data, isMyReply: data.comment_user_id === id };
-            }),
-            createdAt: outerData.created_at,
-          };
-        } else {
-          return {
-            id: outerData.id,
-            nickname: outerData.nickname,
-            profileImage: outerData.profile_image,
-            content: outerData.content,
-            isMyPost: id === outerData.user_id,
-            createdAt: outerData.created_at,
-          };
-        }
-      }),
+      data: {
+        id: viewThread.id,
+        nickname: viewThread.nickname,
+        profileImage: viewThread.profile_image,
+        isMyPost: id === viewThread.user_id,
+        commentCount: viewThreadComment.length,
+        coments: viewThreadComment.map((data) => {
+          return { ...data, isMyReply: data.comment_user_id === id };
+        }),
+        createdAt: viewThread.created_at,
+      },
     };
   } catch (err) {
     console.log(err);
